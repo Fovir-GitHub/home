1f43773e91d346ac542ac7c9691b16cbf4e10b4c
Fovir <fovir@disroot.org>
2025-09-16 14:31:26 +0800

    feat: Add formatter.

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     devShells.${system}.default = pkgs.mkShell {
       # Add packages here.
       buildInputs = with pkgs; [
+        nodejs
         hugo
         just
       ];
@@ -21,6 +22,7 @@
       shellHook = ''
         echo "Entering the development environment!"
         hugo version
+        npm install --no-save prettier prettier-plugin-go-template
         trap 'echo "Leaving the development environment!"' EXIT
       '';
     };
